627d4d2cd818bf643c5a567de2d93e321c5f8fbf
Sahayanathan George Dilen <139237758+dilen1999@users.noreply.github.com>
2024-07-20 22:52:25 +0530

    Add get and post method

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -3,30 +3,53 @@ using GameStore.Api.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<GameDto> games = [
-    new (
-        1, 
-        "Street Fight II",
-        "Fighting ",
-        19.9M,
-        new DateOnly (1999,2,12)
-    ),
-     new (
-        2, 
-        "Street Fight II",
-        "Fighting ",
-        12.9M,
-        new DateOnly (1999,2,12)
-    ),
-     new (
-        3, 
-        "Street Fight II",
-        "Fighting ",
-        59.9M,
-        new DateOnly (1999,2,12)
-    )
-];
+const string GetGameEndpointName = "GetGame";
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = new()
+{
+    new GameDto(
+        1,
+        "Street Fighter II",
+        "Fighting",
+        19.9M,
+        new DateOnly(1999, 2, 12)
+    ),
+    new GameDto(
+        2,
+        "Street Fighter II",
+        "Fighting",
+        12.9M,
+        new DateOnly(1999, 2, 12)
+    ),
+    new GameDto(
+        3,
+        "Street Fighter II",
+        "Fighting",
+        59.9M,
+        new DateOnly(1999, 2, 12)
+    )
+};
+
+// GET /games
+app.MapGet("/games", () => games);
+
+// GET /games/1
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
+   .WithName(GetGameEndpointName);
+
+// POST /games
+app.MapPost("/games", (CreateGameDto newGame) =>
+{
+    GameDto game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
 
 app.Run();
